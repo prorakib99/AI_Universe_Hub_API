272b406d6a687149c6f83f0ce5cc63337fd1858d
Your Name <you@example.com>
2023-12-03 21:44:34 +0600

    All Data working dynamically

---
+++ js/app.js
@@ -0,0 +1,111 @@
+const dataLoad = async() => {
+    const url = 'https://openapi.programming-hero.com/api/ai/tools';
+    const res = await fetch(url);
+    const data = await res.json();
+    loadCard(data.data.tools);
+}
+
+const dataLoadById = async id => {
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayModalContent(data.data);
+}
+
+const displayModalContent = data => {
+    console.log(data);
+    document.getElementById('card-description').innerText = data.description;
+    document.getElementById('plan-1').innerText = data.pricing[0].price.substring(0, 10);
+    document.getElementById('plan-2').innerText = data.pricing[1].price.substring(0, 10);
+    document.getElementById('plan-3').innerText = data.pricing[2].price.substring(0, 10);
+
+    document.getElementById('modal-img').src = data.image_link[0];
+    const accuracyContent = document.getElementById('accuracy-content');
+    const getAccuracy = data.accuracy.score;
+    const accuracy = document.getElementById('accuracy');
+    accuracy.classList.add('d-none')
+    if (getAccuracy !== null) {
+        accuracy.classList.remove('d-none')
+        accuracyContent.innerText = getAccuracy;
+    }
+
+    document.getElementById('about-description').innerText = data.input_output_examples[0].input;
+    document.getElementById('about-description').innerText = data.input_output_examples[0].output;
+
+    const features = data.features
+    const featureContainer = document.getElementById('features-container');
+    featureContainer.innerText = '';
+    for(const feature in features){
+        const featureContent = features[feature].feature_name;
+        const li = document.createElement('li');
+        li.innerText = featureContent;
+        featureContainer.appendChild(li);
+    }
+
+
+    const integrations = data.integrations;
+    const integrationsContainer = document.getElementById('integrations');
+    integrationsContainer.innerText = '';
+    integrations.forEach(integration => {
+        const li = document.createElement('li');
+        li.innerText = integration;
+        integrationsContainer.appendChild(li);
+    })
+
+}
+
+
+const loadCard = cards => {
+    // console.log(cards);
+    const cardContainer = document.getElementById('card-container');
+
+    cards.forEach( (card, i) => {
+        // console.log(i, card);
+        // console.log(card);
+
+        // Image Status code check
+
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('col-md-6', 'col-lg-4', 'd-flex', 'align-items-stretch');
+        cardDiv.innerHTML = `
+            <div class="card p-4">
+            <img class="img-fluid rounded-4 card-img-top card-img" src="${card.image}" alt="...">
+            <div class="card-body px-0">
+            <h5 class="card-title text-black fw-semibold fs-4">Features</h5>
+            <ol class="feature-list-${i} list-group-numbered ps-0 d-flex flex-column gap-1 mt-3 mb-2 text-secondary">
+               
+            </ol>
+            </div>
+            <div class="card-footer border-top border-2 bg-transparent px-0 pb-0 pt-3">
+            <div class="d-flex align-items-center justify-content-between">
+                <div class="d-flex flex-column gap-3">
+                    <h2 class="mb-0 fs-4 fw-semibold">${card.name}</h2>
+                    <div class="d-flex align-content-center gap-2">
+                        <i class="fa-solid fa-calendar-days text-secondary fa-fw fs-5"></i>
+                        <p class="mb-0">${card.published_in}</p>
+                    </div>
+                </div>
+                <button onclick="dataLoadById('${card.id}')" class="btn rounded-circle  details-btn" data-bs-toggle="modal" data-bs-target="#universe-details">
+                    <img src="./assets/images/Frame.png" alt="">
+                </button>
+            </div>
+            </div>
+        </div>
+        `;
+
+        cardContainer.appendChild(cardDiv);
+
+        const features = card.features
+        const featureList = document.querySelector(`.feature-list-${i}`);
+        for(const feature of features){
+            const li = document.createElement('li');
+            li.classList.add('list-group-item');
+            li.innerText = feature;
+            featureList.appendChild(li);
+        }
+
+    });
+}
+
+
+dataLoad()
